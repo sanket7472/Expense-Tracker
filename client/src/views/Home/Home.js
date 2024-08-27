03f55f35ea927ca618957c17c0d0f719f24b5be0
sanket7472 <sankettamnar2003@gmail.com>
2024-08-27 19:58:11 +0530

    working

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -44,20 +44,21 @@ function Home() {
   }, [user])
 
   useEffect(() => {
-    let income = 0
-    let expense = 0
-
-    transactions.forEach((transaction) => {
-      if (transaction.type === 'credit') {
-        income += transaction.amount
-      }
-      else {
-        expense += transaction.amount
-      }
-    })
-
-    setNetIncome(income)
-    setNetExpense(expense)
+    if (transactions && transactions.length > 0) {
+      let income = 0
+      let expense = 0
+  
+      transactions.forEach((transaction) => {
+        if (transaction.type === 'credit') {
+          income += transaction.amount
+        } else {
+          expense += transaction.amount
+        }
+      })
+  
+      setNetIncome(income)
+      setNetExpense(expense)
+    }
   }, [transactions])
 
   return (
@@ -108,24 +109,22 @@ function Home() {
       </div>
 
       <div className='transactionsContainer'>
-        {
-          transactions.map((transaction) => {
+  
+  {transactions && transactions.map((transaction) => {
+    const { _id, title, amount, category, type, createdAt } = transaction
 
-            const { _id, title, amount, category, type, createdAt } = transaction
-
-            return (<TransactionCard
-              Key={_id}
-              _id={_id}
-              title={title}
-              amount={amount}
-              category={category}
-              type={type}
-              createdAt={createdAt}
-              loadTransactions={loadTransactions}
-            />
-            )
-          })
-        }
+    return (<TransactionCard
+      Key={_id}
+      _id={_id}
+      title={title}
+      amount={amount}
+      category={category}
+      type={type}
+      createdAt={createdAt}
+      loadTransactions={loadTransactions}
+    />
+    )
+  })}
       </div>
       <Link to='/add-transaction' className='add-transaction-Link'>
         <img src={ImgAdd}
@@ -135,7 +134,7 @@ function Home() {
       </Link>
       <Toaster />
     </div>
-  )
-}
+  )}
+
 
 export default Home
